62f4c37bcb3011fc71185204f8197c8ba51e1d6e
Martin Jakubik <martin@jakubik.info>
2016-11-20 15:04:30 +0100

    Detect the language and print the progress
    Only add english tweets.

--- put_into_database.py
+++ put_into_database.py
@@ -2,6 +2,8 @@ import sys
 import datetime
 import sql
 import psycopg2
+import nltk
+from nltk.corpus import stopwords
 import helpers
 
 
@@ -14,10 +16,22 @@ def process_file(filename, database):
     tweet_user = None
     tweet_content = None
 
-    # read the tweets
-    tweet_number = 0
+    # stats
     first_line = True
+    total_amount = 0
+    tweet_count = 0
+    english_tweets = 0
+    inserted_tweets = 0
+
+    # read the tweets
     for line in helpers.lazy_read_file(filename):
+        # first line contains the number of tweets
+        if first_line:
+            first_line = False
+            pos = line.rfind(':')
+            total_amount = int(line[(pos + 1):-1])
+            continue
+
         # is this an attribute?
         if len(line) < 3:
             continue
@@ -34,17 +48,36 @@ def process_file(filename, database):
         elif attr == 'W':
             tweet_content = line[2:-1]
 
-            # add to a database
-            tweets = sql.Table('tweets')
-            query = tuple(tweets.insert(columns = [tweets.timestamp, tweets.user, tweets.content],
-                                        values = [[tweet_timestamp, tweet_user, tweet_content]]))
-            cur.execute(query[0], query[1])
-            database.commit()
+            # print progress
+            if tweet_count % 5000 == 0:
+                print('Processed %.2f%% of tweets' % (tweet_count / total_amount * 100))
 
-            tweet_number += 1
-        elif first_line:
-            # first line contains the tweet number, ignore
-            first_line = False
+            # check the language
+            tokens = nltk.wordpunct_tokenize(tweet_content)
+            words = set(word.lower() for word in tokens)
+
+            ratios = {}
+            for language in stopwords.fileids():
+                stopwords_set = set(stopwords.words(language))
+                common_words = words.intersection(stopwords_set)
+                ratios[language] = len(common_words)
+
+            tweet_language = max(ratios, key = ratios.get)
+            if tweet_language == 'english':
+                english_tweets += 1
+
+                # add to a database
+                tweets = sql.Table('tweets')
+                query = tuple(tweets.insert(columns = [tweets.timestamp, tweets.user, tweets.content],
+                                            values = [[tweet_timestamp, tweet_user, tweet_content]]))
+                try:
+                    cur.execute(query[0], query[1])
+                    database.commit()
+                    inserted_tweets += 1
+                except:
+                    print('Failed to insert the tweet %d into the database' % tweet_count)
+
+            tweet_count += 1
         else:
             print('Unknown tweet attribute %s' % attr)
             exit(1)
@@ -52,8 +85,13 @@ def process_file(filename, database):
     # close the cursor
     cur.close()
 
-    # stats
-    print('Inserted %d tweets into the database from file %s' % (tweet_number, filename))
+    # print stats
+    print()
+    print('Stats:')
+    print('Dataset: %s' % filename)
+    print('Total tweets: %d' % tweet_count)
+    print('English tweets: %d' % english_tweets)
+    print('Inserted tweets: %d' % inserted_tweets)
 
 
 # connect to postgres
