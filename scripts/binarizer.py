6ff6e5c0eb30de8485ab9245a9bb1871fc0fe5a4
Martin Jakubik <martin@jakubik.info>
2017-04-14 17:28:37 +0200

    Add a script for processing and binarizing columns

--- scripts/binarizer.py
+++ scripts/binarizer.py
@@ -1,20 +1,92 @@
 import numpy as np
 import pandas as pd
 import sys
+from datetime import datetime
 from sklearn.preprocessing import MultiLabelBinarizer
 
 
-def binarize(data: pd.SparseDataFrame, column: str):
-    """ Binarize a certain column. """
+def prepare_dataset():
+    """ Pre-process the raw dataset and prepare it
+        for binarizing. Loads data.csv and saves the result
+        to data_prepared.pkl. """
 
-    print('\n\nBinarizing column ' + column)
+    print('Preparing the dataset')
+
+    print('Loading data.csv')
+    data: pd.DataFrame = pd.read_csv('../data/data.csv')
+
+    # convert timestamps to week numbers
+    print('Converting weeks')
+    data['week'] = data['week'].apply(lambda w: datetime.strptime(w, '%Y-%m-%d 00:00:00').isocalendar()[1])
+
+    # drop tweets from 43th week
+    data: pd.DataFrame = data[data['week'] < 40]
+
+    # drop unused columns
+    data.drop(['total_length', 'total_words'], axis=1, inplace=True)
+
+    # process categorical columns
+    for c in ['hashtags', 'mentions', 'urls']:
+        print('Processing lists in ' + c)
+        data[c] = data[c].apply(lambda s: s[1:-1])
+
+    print('The shape of the data is %d by %d' % data.shape)
+
+    # save to a CSV
+    print('Saving as prepared_data.csv')
+    data.to_csv('../data/prepared_data.csv', index=False)
+
+
+def pivot_dataset():
+    """ Make a pivot table from the dataset. """
+
+    print('Pivoting the dataset')
+
+    # load the data
+    print('Loading prepared_data.csv')
+    data: pd.DataFrame = pd.read_csv('../data/prepared_data.csv')
+
+    # process categorical columns - split to lists
+    for c in ['hashtags', 'mentions', 'urls']:
+        print('Processing lists in ' + c)
+        data[c] = data[c].apply(lambda s: s.split(',') if type(s) == str else [])
+
+    # make the pivot table
+    print('Making the pivot table')
+    pivot: pd.DataFrame = data.set_index(['user', 'week']).unstack('week')
+    del data
+
+    # fill missing tweets
+    print('Filling NAs in tweets')
+    pivot['tweets'] = pivot['tweets'].fillna(0)
+
+    # process categorical columns again - join back to strings
+    for c in ['hashtags', 'mentions', 'urls']:
+        print('Processing lists in ' + c)
+        pivot[c] = pivot[c].apply(lambda s: ','.join(s) if type(s) == list else '')
+
+    print('The shape of the pivot table is %d by %d' % pivot.shape)
+
+    # save to a CSV
+    print('Saving as pivot_data.csv')
+    pivot.to_csv('../data/pivot_data.csv', index=True, index_label='user')
+
+
+def binarize_dataset():
+    """ Binarize the whole dataset. Use the prepared data
+        and save the binarized data. """
+
+    pass
+
+
+def binarize(data: pd.SparseDataFrame, column: str):
+    """ Binarize a certain column. Save the new columns
+        back to the provided data frame, in place. """
 
     # use the label binarizer to get the new columns
-    print('Using MultiLabelBinarizer')
     bin_columns = MultiLabelBinarizer(sparse_output=True).fit_transform(data[column].apply(lambda x: x.split('|')))
 
     # filter only most used values
-    print('Filtering columns')
     usage = bin_columns.sum(0)
     columns_with_high_usage = bin_columns[:, np.where(usage >= 200)[0]]
     columns_with_low_usage = bin_columns[:, np.where(usage < 200)[0]]
@@ -22,63 +94,27 @@ def binarize(data: pd.SparseDataFrame, column: str):
 
     # insert back to the dataset
     num_columns = columns_with_high_usage.shape[1]
-    print('Inserting ' + str(num_columns + 1) + ' columns')
 
-    print('Inserting other column')
     data.drop(column, axis=1, inplace=True)
     data[column + '_other'] = pd.SparseSeries(np.asarray(has_other).ravel(), fill_value=0)
 
     for i in range(num_columns):
-        print('Inserting column', i, '/', num_columns)
         data[column + '_' + str(i)] = pd.SparseSeries(columns_with_high_usage[:, i].toarray().ravel(), fill_value=0)
-
-
-def binarize_hashtags():
-    """ Convert hashtags column to N binary columns. """
-
-    print('Loading hashtags')
-    data = pd.read_csv('../data/hashtags_for_binarizer.csv').to_sparse(fill_value=0)
-    binarize(data, 'hashtags')
-    print('Saving hashtags')
-    data.to_pickle('../data/hashtags_binarized.pkl')
-
-
-def binarize_mentions():
-    """ Convert mentions column to N binary columns. """
-
-    print('Loading mentions')
-    data = pd.read_csv('../data/mentions_for_binarizer.csv').to_sparse(fill_value=0)
-    binarize(data, 'mentions')
-    print('Saving mentions')
-    data.to_pickle('../data/mentions_binarized.pkl')
-
-
-def binarize_urls():
-    """ Convert urls column to N binary columns. """
-
-    print('Loading urls')
-    data = pd.read_csv('../data/urls_for_binarizer.csv').to_sparse(fill_value=0)
-    binarize(data, 'urls')
-    print('Saving urls')
-    data.to_pickle('../data/urls_binarized.pkl')
-
-
-def binarize_dataset():
-    """ Load the dataset, prepare it, and convert categorical
-        columns to N columns with 1 and 0. This script presumes
-        there are already separate CSVs for hashtags, mentions, and URLs. """
-
-    binarize_hashtags()
-    binarize_mentions()
-    binarize_urls()
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
+        # by default, binarize the dataset
         binarize_dataset()
-    elif sys.argv[1] == 'hashtags':
-        binarize_hashtags()
-    elif sys.argv[1] == 'mentions':
-        binarize_mentions()
-    elif sys.argv[1] == 'urls':
-        binarize_urls()
+
+    elif sys.argv[1] == 'binarize':
+        binarize_dataset()
+
+    elif sys.argv[1] == 'prepare':
+        prepare_dataset()
+
+    elif sys.argv[1] == 'pivot':
+        pivot_dataset()
+
+    else:
+        print('Unknown action: ' + sys.argv[1])
